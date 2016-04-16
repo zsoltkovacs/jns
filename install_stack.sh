3d01100c4cdc597bc0b94a355ecf671782300a59
Zsolt KOVACS <zsoltkovacs@users.noreply.github.com>
2016-04-16 19:15:42 +0200

    Update install_stack.sh
    Added boken and TensoFlow support

--- install_stack.sh
+++ install_stack.sh
@@ -21,6 +21,7 @@ pip install xlwt
 pip install XlsxWriter
 pip install beautifulsoup4
 pip install html5lib
+pip install bokeh
 
 #------------------------------------------------------
 apt-get -y install libxml2-dev libxslt-dev
@@ -37,3 +38,7 @@ apt-get -y install libatlas-base-dev gfortran
 #-----------------------------------------------------
 
 pip install scipy
+
+#-----------------------------------------------------
+# install TensorFlow
+pip install https://github.com/samjabrahams/tensorflow-on-raspberry-pi/raw/master/bin/tensorflow-0.7.1-cp27-none-linux_armv7l.whl
